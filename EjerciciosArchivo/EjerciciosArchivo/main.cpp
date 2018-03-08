66a9048ad33f603317cc16e2c7be7b0e4b87a210
vincentsbarrios <vincent.sbarrios@gmail.com>
2018-03-08 11:11:31 -0600

    TAREA-ARCHIVOS-PROMEDIO
    .

--- EjerciciosArchivo/EjerciciosArchivo/main.cpp
+++ EjerciciosArchivo/EjerciciosArchivo/main.cpp
@@ -1,22 +1,56 @@
 #include <conio.h>
 #include "Notas.h"
+#include <iostream>
 
+using namespace std;
 
 int main() {
 
-	//Notas::registrarAlumnos();
-	//Notas::consultarAlumnos();
+	cout << "* M E N U  P R I N C I P A L *" << endl;
+	int opp = 0;
 
-	//Notas::registrarMaterias();
-	//Notas::consultarMateria();
+	do {
+		cout << "\n1. Registrar Alumno" << endl;
+		cout << "2. Registrar Materia" << endl;
+		cout << "3. Registrar Notas" << endl;
+		cout << "4. Consultar Alumnos" << endl;
+		cout << "5. Consultar Materias" << endl;
+		cout << "6. Consultar Notas" << endl;
+		cout << "7. Promedio de Alumno" << endl;
+		cout << "8. Promedio de Materias" << endl;
+		cout << "9. Salir" << endl;
+		cout << "------> ";
+		cin >> opp;
 
-	//Notas::registrarNotas();
+		switch (opp) {
+		case 1:
+			Notas::registrarAlumnos();
+			break;
+		case 2:
+			Notas::registrarMaterias();
+			break;
+		case 3:
+			Notas::registrarNotas();
+			break;
+		case 4:
+			Notas::consultarAlumnos();
+			break;
+		case 5:
+			Notas::consultarMateria();
+			break;
+		case 6:
+			Notas::consultarNota();
+			break;
+		case 7:
+			Notas::promedioAlumno();
+			break;
+		case 8:
+			Notas::promedioMateria();
+			break;
+		}
 
-	Notas::consultarNota();
+	} while (opp != 9);
+	
 
-	Notas::promedioAlumno();
-	Notas::promedioMateria();
-
-	_getch();
 	return 0;
 }
